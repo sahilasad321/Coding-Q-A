5a78bbf74033098185a74995c949343001a637cc
Sahil Asad <sahilasad321@gmail.com>
2024-02-03 15:01:29 +0530

    Code Added

---
+++ Mid_Point_of_Line.py
@@ -0,0 +1,11 @@
+# Write a Python program to help Harry to find the mid-point of line.
+
+x1 = float(input())
+y1 = float(input())
+
+x2 = float(input())
+y2 = float(input())
+
+mid_point = (x1 + x2)/2 , (y1 + y2)/2
+
+print("Midpoint of Line :" , mid_point)
