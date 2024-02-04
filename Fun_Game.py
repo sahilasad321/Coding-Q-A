cf2902465cca9e49727e5f9494fc0c63b960ad8a
Sahil Asad <sahilasad321@gmail.com>
2024-02-04 14:18:06 +0530

    Added code

---
+++ Fun_Game.py
@@ -0,0 +1,13 @@
+# Kids on a picnic decided to play fun game and find the area of circle they are sitting on. 
+# Assume the radius of circle to be some floating numbers.
+
+# Area of a Circle
+
+# In geometry, the area enclosed by a circle of radius r is πr2. 
+# Here the Greek letter π represents a constant, approximately equal to 3.14159, 
+# which is equal to the ratio of the circumference of any circle to its diameter.
+
+radius = float(input())
+Area_of_circle = 3.14159 * radius * radius
+
+print("The area of circle is :", Area_of_circle)
